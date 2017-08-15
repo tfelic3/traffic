5a9a13692132266c53f9d701b85d1a6270812d4a
tfelic3 <felicien.tesalon@gmail.com>
2017-08-15 04:48:13 -0400

    Update fatality.js

--- fatalities/script/fatality.js
+++ fatalities/script/fatality.js
@@ -1,108 +1,306 @@
+$('.arrow').on('click', function(){
+	$('#intro').hide(300);
+	$('#graphic').fadeIn(600);
+});
+
 
 $('#begin').on('click', function(){
-	$("#car, #car2,#car3").animate({left: '1%'});});
-
+	$("#car, #car2,#car3, #car4, #car5,#car6,#car7, #car8,#car9,#car10").animate({left: '1%'});
+$('.counter, .counter2, .counter3, .counter4, .counter5, .counter6, .counter7, .counter8, .counter9, .counter10').html(0+" "+"Fatalities")});
 
 $('#january').on('click', function(){
-$('.counter').html(greenvilleFatals.january);
-$('.counter2').html(spartanburgFatals.january);
-$('.counter3').html(andersonFatals.january);
+$('.counter').html(greenvilleFatals.january + " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.january + " "+ "Fatalities");
+$('.counter3').html(andersonFatals.january + " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.january + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.january+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.january+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.january+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.january+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.january+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.january+ " "+ "Fatalities");
 $("#car2").animate({left: '5%'});
-$("#car").animate({left: '7%'})
-$("#car3").animate({left: '7%'});});
-
+$("#car").animate({left: '7%'});
+$("#car3").animate({left: '7%'});
+$("#car4").animate({left: '3%'});
+$("#car5").animate({left: '3%'});
+$("#car6").animate({left: '3%'});
+$("#car7").animate({left: '3%'});
+$("#car8").animate({left: '5%'});
+$("#car9").animate({left: '3%'});
+$("#car10").animate({left: '3%'});});
 
 
 $('#february').on('click', function(){
-$('.counter').html(greenvilleFatals.february);
-$('.counter2').html(spartanburgFatals.february);
-$('.counter3').html(andersonFatals.february);
+$('.counter').html(greenvilleFatals.february+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.february+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.february+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.february + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.february+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.february+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.february+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.february+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.february+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.february+ " "+ "Fatalities");
 $('#car2').animate({left: '13%'});
-$("#car").animate({left: '12.5%'});
-$("#car3").animate({left: '11.5%'});});
+$("#car").animate({left: '12%'});
+$("#car3").animate({left: '10%'});
+$("#car4").animate({left: '10%'});
+$("#car5").animate({left: '9%'});
+$("#car6").animate({left: '9%'});
+$("#car7").animate({left: '9%'});
+$("#car8").animate({left: '8%'});
+$("#car9").animate({left: '8%'});
+$("#car10").animate({left: '8%'});});
 
 
 $('#march').on('click', function(){
-$('.counter').html(greenvilleFatals.march);
-$('.counter2').html(spartanburgFatals.march);
-$('.counter3').html(andersonFatals.march);
-$('#car2').animate({left: '18.5%'});
-$("#car").animate({left: '19%'});
-$("#car3").animate({left: '19%'});});
+$('.counter').html(greenvilleFatals.march+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.march+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.march+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.march + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.march+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.march+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.march+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.march+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.march+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.march+ " "+ "Fatalities");
+$("#car").animate({left: '20%'});
+$('#car2').animate({left: '18%'});
+$("#car3").animate({left: '18%'});
+$("#car4").animate({left: '15%'});
+$("#car5").animate({left: '16%'});
+$("#car6").animate({left: '16%'});
+$("#car7").animate({left: '15%'});
+$("#car8").animate({left: '15%'});
+$("#car9").animate({left: '17%'});
+$("#car10").animate({left: '16%'});});
+
 
 $('#april').on('click', function(){
-	$('.counter2').html(spartanburgFatals.april);
-	$('.counter3').html(andersonFatals.april);
-	$('.counter').html(greenvilleFatals.april);
-	$('#car2').animate({left: '24%'});
-	$("#car").animate({left: '26%'})
-	$("#car3").animate({left: '26%'});});
+$('.counter').html(greenvilleFatals.april+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.april+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.april+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.april + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.april+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.april+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.april+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.april+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.april+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.april+ " "+ "Fatalities");
+$("#car").animate({left: '26%'});
+$('#car2').animate({left: '22%'});
+$("#car3").animate({left: '20%'});
+$("#car4").animate({left: '18%'});
+$("#car5").animate({left: '19%'});
+$("#car6").animate({left: '19%'});
+$("#car7").animate({left: '18%'});
+$("#car8").animate({left: '19%'});
+$("#car9").animate({left: '20%'});
+$("#car10").animate({left: '20%'});});
 
 $('#may').on('click', function(){
-	$('.counter2').html(spartanburgFatals.may);
-	$('.counter3').html(spartanburgFatals.may);
-	$('#car2').animate({left: '31.5%'});
-	$('.counter').html(greenvilleFatals.may);
-	$("#car").animate({left: '32.5%'});
-$("#car3").animate({left: '31.5%'});});
+$('.counter').html(greenvilleFatals.may+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.may+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.may+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.may + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.may+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.may+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.may+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.may+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.may+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.may+ " "+ "Fatalities");
+$('#car').animate({left: '32%'});	
+$("#car2").animate({left: '31%'});
+$("#car3").animate({left: '31%'});
+$("#car4").animate({left: '27%'});
+$("#car5").animate({left: '31%'});
+$("#car6").animate({left: '29%'});
+$("#car7").animate({left: '30%'});
+$("#car8").animate({left: '27%'});
+$("#car9").animate({left: '27%'});
+$("#car10").animate({left: '30%'});});
 
 $('#june').on('click', function(){
-	$('.counter2').html(spartanburgFatals.june);
-	$('.counter3').html(andersonFatals.june);
-	$('#car2').animate({left: '36%'});
-	$('.counter').html(greenvilleFatals.june);
-	$("#car").animate({left: '38%'})
-	$("#car3").animate({left: '37%'});});
+$('.counter').html(greenvilleFatals.june+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.june+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.june+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.june + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.june+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.june+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.june+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.june+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.june+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.june+ " "+ "Fatalities");
+$('#car').animate({left: '39%'});
+$("#car2").animate({left: '35%'})
+$("#car3").animate({left: '37%'});
+$("#car4").animate({left: '32%'});
+$("#car5").animate({left: '35%'});
+$("#car6").animate({left: '32%'});
+$("#car7").animate({left: '32%'});
+$("#car8").animate({left: '32%'});
+$("#car9").animate({left: '35%'});
+$("#car10").animate({left: '34%'});});
 
 $('#july').on('click', function(){
-	$('.counter2').html(spartanburgFatals.july);
-	$('.counter3').html(andersonFatals.july);
-	$('#car2').animate({left: '42%'});
-	$('.counter').html(greenvilleFatals.july);
-	$("#car").animate({left: '45%'});
-    $("#car3").animate({left: '42.5%'});});
+$('.counter').html(greenvilleFatals.july+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.july+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.july+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.july + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.july+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.july+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.july+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.july+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.july+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.july+ " "+ "Fatalities");
+$('#car').animate({left: '45%'});
+$("#car2").animate({left: '39%'});
+$("#car3").animate({left: '40%'});
+$("#car4").animate({left: '35%'});
+$("#car5").animate({left: '36%'});
+$("#car6").animate({left: '35%'});
+$("#car7").animate({left: '35%'});
+$("#car8").animate({left: '36%'});
+$("#car9").animate({left: '37%'});
+$("#car10").animate({left: '35%'});});
 
 $('#august').on('click', function(){
-	$('.counter2').html(spartanburgFatals.august);
-	$('.counter3').html(andersonFatals.august);
-	$('#car2').animate({left: '51%'});
-	$('.counter').html(greenvilleFatals.august);
-	$("#car").animate({left: '49%'});
-$("#car3").animate({left: '52%'});});
+	$('.counter').html(greenvilleFatals.august+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.august+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.august+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.august + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.august+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.august+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.august+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.august+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.august+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.august+ " "+ "Fatalities");
+$("#car").animate({left: '48%'});
+$('#car2').animate({left: '50%'});
+$("#car3").animate({left: '52%'});
+$("#car4").animate({left: '46%'});
+$("#car5").animate({left: '47%'});
+$("#car6").animate({left: '46%'});
+$("#car7").animate({left: '49%'});
+$("#car8").animate({left: '48%'})
+$("#car9").animate({left: '46%'})
+$("#car10").animate({left: '46%'});});
+
 
 $('#september').on('click', function(){
-	$('.counter2').html(spartanburgFatals.september);
-	$('.counter3').html(andersonFatals.september);
-	$('#car2').animate({left: '55%'});
-	$('.counter').html(greenvilleFatals.september);
-	$("#car").animate({left: '56%'})
-	$("#car3").animate({left: '57%'});});
+$('.counter').html(greenvilleFatals.september+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.september+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.september+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.september + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.september+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.september+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.september+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.september+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.september+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.september+ " "+ "Fatalities");
+$("#car").animate({left: '57%'});
+$('#car2').animate({left: '52%'});
+$("#car3").animate({left: '58%'});
+$("#car4").animate({left: '51%'});
+$("#car5").animate({left: '52%'});
+$("#car6").animate({left: '51%'});
+$("#car7").animate({left: '51%'});
+$("#car8").animate({left: '51%'})
+$("#car9").animate({left: '52%'})
+$("#car10").animate({left: '51%'});});
 
 $('#october').on('click', function(){
-	$('.counter2').html(spartanburgFatals.october);
-	$('.counter3').html(andersonFatals.october);
-	$('#car2').animate({left: '63%'});
-	$('.counter').html(greenvilleFatals.october);
-	$("#car").animate({left: '64%'});
-    $("#car3").animate({left: '62.5%'});});
+$('.counter').html(greenvilleFatals.october+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.october+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.october+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.october + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.october+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.october+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.october+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.october+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.october+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.october+ " "+ "Fatalities");
+$("#car").animate({left: '63%'});
+$('#car2').animate({left: '62%'});
+$("#car3").animate({left: '61%'});
+$("#car4").animate({left: '56%'});
+$("#car5").animate({left: '58%'});
+$("#car6").animate({left: '57%'});
+$("#car7").animate({left: '56%'});
+$("#car8").animate({left: '56%'})
+$("#car9").animate({left: '57%'})
+$("#car10").animate({left: '57%'});});
 
 
 $('#november').on('click', function(){
-	$('.counter2').html(spartanburgFatals.november);
-	$('.counter3').html(andersonFatals.november);
-	$('#car2').animate({left: '67%'});
-	$('.counter').html(greenvilleFatals.november);
-	$("#car").animate({left: '70%'});
-    $("#car3").animate({left: '67.5%'});});
+$('.counter').html(greenvilleFatals.november+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.november+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.november+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.november + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.november+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.november+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.november+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.november+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.november+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.november+ " "+ "Fatalities");
+$('#car').animate({left: '69%'});
+$("#car2").animate({left: '63%'});
+$("#car3").animate({left: '64%'});
+$("#car4").animate({left: '60%'});
+$("#car5").animate({left: '60%'});
+$("#car6").animate({left: '59%'});
+$("#car7").animate({left: '60%'});
+$("#car8").animate({left: '60%'})
+$("#car9").animate({left: '61%'})
+$("#car10").animate({left: '61%'});});
 
 
 $('#december').on('click', function(){
-	$('.counter2').html(spartanburgFatals.december);
-	$('.counter3').html(andersonFatals.december);
-	$('#car2').animate({left: '72%'});
-	$('.counter').html(greenvilleFatals.december);
-	$("#car").animate({left: '76%'});
-    $("#car3").animate({left: '70%'});});
+$('.counter').html(greenvilleFatals.december+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.december+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.december+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.december + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.december+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.december+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.december+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.december+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.december+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.december+ " "+ "Fatalities");
+$("#car").animate({left: '75%'});
+$('#car2').animate({left: '70%'});
+$("#car3").animate({left: '67%'});
+$("#car4").animate({left: '67%'});
+$("#car5").animate({left: '68%'});
+$("#car6").animate({left: '68%'});
+$("#car7").animate({left: '67%'});
+$("#car8").animate({left: '67%'})
+$("#car9").animate({left: '73%'})
+$("#car10").animate({left: '67%'});});
+
+
+$('#total').on('click', function(){
+$('.counter').html(greenvilleFatals.total+ " "+ "Fatalities");
+$('.counter2').html(spartanburgFatals.total+ " "+ "Fatalities");
+$('.counter3').html(andersonFatals.total+ " "+ "Fatalities");
+$('.counter4').html(abbevilleFatals.total + " "+ "Fatalities");
+$('.counter5').html(pickensFatals.total+ " "+ "Fatalities");
+$('.counter6').html(oconeeFatals.total+ " "+ "Fatalities");
+$('.counter7').html(cherokeeFatals.total+ " "+ "Fatalities");
+$('.counter8').html(unionFatals.total+ " "+ "Fatalities");
+$('.counter9').html(laurensFatals.total+ " "+ "Fatalities");
+$('.counter10').html(greenwoodFatals.total+ " "+ "Fatalities");
+$('#car').animate({left: '81%'});
+$("#car2").animate({left: '77%'});
+$("#car3").animate({left: '79%'});
+$("#car4").animate({left: '73%'});
+$("#car5").animate({left: '75%'});
+$("#car6").animate({left: '74%'});
+$("#car7").animate({left: '71%'});
+$("#car8").animate({left: '72%'})
+$("#car9").animate({left: '76%'})
+$("#car10").animate({left: '74%'});});
+
 
 
 
@@ -269,8 +467,13 @@ december:0,
 total:11,
 };
 
+var months =['#begin','#january', '#february', '#march','#april', '#may','#june','#july','#august','#september','#october','#november','#december', '#total'];
 
-
-
-
-
+var num =0;
+var interval=setInterval(function(){
+	$(months[num]).click();
+	num++;
+	if (num===months.length) {
+		clearInterval(interval)
+	}
+}, 3000);
